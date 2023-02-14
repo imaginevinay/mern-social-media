ebdb3b29405e0ecf232d28b9a95996161cac4613
Vinay Varshney <vinay.varshney@nagarro.com>
2023-02-14 23:12:45 +0530

    snackbar state mgmt for onclose fixed

--- client/src/commons/Snackbar/index.jsx
+++ client/src/commons/Snackbar/index.jsx
@@ -1,6 +1,8 @@
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
 import { forwardRef } from "react";
+import { useDispatch } from "react-redux";
+import { setSnackBar } from "../../state";
 
 
 const Alert = forwardRef(function Alert(props, ref) {
@@ -13,9 +15,10 @@ const SnackbarComponent = ({
   snackbarType,
   children,
 }) => {
+  const dispatch = useDispatch();
   return (
     <div>
-      <Snackbar open={openSnackbar} autoHideDuration={6000}>
+      <Snackbar open={openSnackbar} autoHideDuration={6000} onClose={() => dispatch(setSnackBar({isOpenSnackbar : false}))}>
         {snackbarType === "success" ? (
           <Alert severity="success" sx={{ width: "100%" }}>
             {message}
